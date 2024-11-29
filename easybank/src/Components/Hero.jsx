2821b64748357b6b886df8498dd00e50832608be
Ifeoluwa <iamsamnice@gmail.com>
2024-11-29 13:31:43 -0800

    almost FIXED

--- easybank/src/Components/Hero.jsx
+++ easybank/src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import IconBG from '../assets/Icon/IconBG.jsx'
 function Hero() {
 	return (
 		<div className='max-w-7xl mx-auto h-full grid md:grid-cols-2'>
-			<div className='order-last flex flex-col items-center text-center lg:items-start justify-center md:order-first mx-auto max-w-lg p-8 h-full'>
+			<div className='order-last flex flex-col text-center md:text-left lg:items-start justify-center md:order-first mx-auto max-w-lg p-8 h-full'>
 				<p className='text-5xl lg:text-6xl text-gray-800'>
 					Next generation digital banking
 				</p>
@@ -25,7 +25,7 @@ function Hero() {
 
 			<div className='order-first md:order-last h-[26rem] md:h-full'>
 				<div>
-					<IconBG className='absolute -top-60 w-[1000px] -left-16 md:-right-64 md:top-20 md:w-[850px] lg:w-[1100px] lg:-top-28 lg:-right-72' />
+					<IconBG className='absolute -top-60 w-[1000px] -left-16 md:-right-64 md:top-20 md:w-[850px] lg:w-[1100px] lg:-top-28 lg:-right-72 md:left-auto' />
 				</div>
 			</div>
 		</div>
